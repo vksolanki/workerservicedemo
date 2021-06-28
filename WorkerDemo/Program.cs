687ab1908abe3196a5b4e23767286a83d42597a5
Vikas Solanki <48885907+vksolanki@users.noreply.github.com>
2021-06-28 17:54:52 +0530

    Update Program.cs

--- WorkerDemo/Program.cs
+++ WorkerDemo/Program.cs
@@ -16,6 +16,7 @@ namespace WorkerDemo
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
